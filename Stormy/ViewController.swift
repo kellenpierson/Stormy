0eaddcb39e4c71004a766ae5c326c1b1dc7f4375
Kellen Pierson <kellenpierson@gmail.com>
2016-01-26 15:56:42 -0800

    added temp, humidity and precipProbability

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentTemperatureLabel: UILabel?
+    @IBOutlet weak var humidityPercentageLabel: UILabel?
+    @IBOutlet weak var rainPercentageLabel: UILabel?
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
+        let weatherDictionary = NSDictionary(contentsOfFile: plistPath),
+        let currentWeatherDictionary = weatherDictionary["currently"] as? [String: AnyObject] {
+
+            let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
+
+            currentTemperatureLabel?.text = "\(currentWeather.temperature)º"
+            humidityPercentageLabel?.text = "\(currentWeather.humidity)%"
+            rainPercentageLabel?.text = "\(currentWeather.precipProbability)%"
+
+        }
     }
 
     override func didReceiveMemoryWarning() {
