fee3ebe5da3315f209e883bf2e31aae58a656630
Kellen Pierson <kellenpierson@gmail.com>
2016-01-28 16:22:13 -0800

    added apikeys swift wrapper

---
+++ Stormy/ApiKeys.swift
@@ -0,0 +1,17 @@
+//
+//  ApiKeys.swift
+//  Stormy
+//
+//  Created by Kellen Pierson on 1/28/16.
+//  Copyright © 2016 Kellen Pierson. All rights reserved.
+//
+
+import Foundation
+
+func valueForAPIKey(keyname keyname: String) -> String {
+    let filePath = NSBundle.mainBundle().pathForResource("ApiKeys", ofType: "plist")
+    let plist = NSDictionary(contentsOfFile: filePath!)
+
+    let value: String = plist?.objectForKey(keyname) as! String
+    return value
+}
